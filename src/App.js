f6b7a0f0d23f4b26985efe3eedb4232180425a86
HMoreira2020 <moreiraheather@gmail.com>
2020-12-09 10:42:21 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import { addItem } from  './actions/items';
 class App extends Component {
 
   handleOnClick() {
-    this.props.store.dispatch(addItem());
+    this.props.addItem();
   }
 
   render() {
+    debugger
     return (
       <div className="App">
         <button onClick={(event) => this.handleOnClick(event)}>
@@ -21,10 +22,24 @@ class App extends Component {
   }
 };
 
-const mapStateToProps = (state) => {
-  return {
-    items: state.items
-  };
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     items: state.items
+//   };
+// };
 
-export default connect(mapStateToProps)(App);
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     addItem: () => {
+//       dispatch(addItem())
+//     }
+//   }
+// }
+//export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+export default connect(state => ({ items: state.items }), { addItem })(App);
+//The object just needs to contain key/value pairs for 
+//each action creator we want to become props. (i.e., { addItem: addItem})
+//you can use the shorthand if your key and value are the same 
+//connect incorporates dispatch for us, passing in the key which calls the action creator
+//that is stored in the value dispatch(addItem())
