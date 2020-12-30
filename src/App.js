fba17e55391fcde08e64fe05a0a66b9906df21cc
pledgedavis <pledgedavis14@gmail.com>
2020-12-30 01:18:15 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -5,26 +5,62 @@ import { addItem } from  './actions/items';
 
 class App extends Component {
 
-  handleOnClick() {
-    this.props.store.dispatch(addItem());
+  // handleOnClick() {
+  //   this.props.store.dispatch(addItem());
+  // }
+
+  handleOnClick = event => {
+    this.props.addItem()
   }
 
-  render() {
-    return (
-      <div className="App">
-        <button onClick={(event) => this.handleOnClick(event)}>
-          Click
-          </button>
-        <p>{this.props.items.length}</p>
-      </div>
-    );
-  }
+
+//   render() {
+//     debugger
+//     return (
+//       <div className="App">
+//         <button onClick={(event) => this.handleOnClick(event)}>
+//           Click
+//           </button>
+//         <p>{this.props.items.length}</p>
+//       </div>
+//     );
+//   }
+// };
+
+render() {
+  debugger
+  return (
+    <div className="App">
+      <button onClick={this.handleOnClick}>
+        Click
+        </button>
+      <p>{this.props.items.length}</p>
+    </div>
+  );
+}
 };
 
-const mapStateToProps = (state) => {
-  return {
-    items: state.items
-  };
-};
 
-export default connect(mapStateToProps)(App);
+
+export default connect(state => ({ items: state.items }), { addItem })(App);
+
+// const mapStateToProps = (state) => {
+//   return {
+//     items: state.items
+//   };
+// };
+
+
+// export default connect(mapStateToProps, { addItem })(App); // Code change: no mapDispatchToProps function required!
+
+
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addItem: () => {
+//       dispatch(addItem())
+//     }
+//   };
+// };
+ 
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
